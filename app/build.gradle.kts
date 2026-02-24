c5b3fad8669eed8d2b1d4c643d23b11ab6b1a9db
ephemeral <unsigned57@gmail.com>
2026-02-25 03:34:30 +0800

    chore: Release version 0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,8 +83,8 @@ android {
         applicationId = "com.lomo.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.5.1"
+        versionCode = 18
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
